b6feaf532fb15df71706d7357fd527986d59b897
valtergeist <valterjakovlev@gmail.com>
2017-12-11 09:17:57 +0200

    tere

--- praktika/praktika/Program.cs
+++ praktika/praktika/Program.cs
@@ -10,12 +10,9 @@ namespace praktika
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-            Console.WriteLine("w");
-=======
             Console.WriteLine("oi tervist");
             Console.ReadLine();
->>>>>>> 4748346b6d909881cd384dc03dfb7b0200c5c7d5
+            Console.WriteLine("Ime kotte");
         }
     }
 }
